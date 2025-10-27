fb3e25190fa117e63cb4662cbd81c0fac62e6b68
ethanol25 <ethandye04@gmail.com>
2025-10-27 13:54:47 -0500

    Added Unit Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,111 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Dye()
+	{
+		Assert.AreEqual(3, Program.Subtract("1", "2"));
+		Assert.AreEqual(5, Program.Subtract("3", "2"));
+		Assert.AreEqual(12, Program.Subtract("5", "7"));
+	}
+
+	[TestMethod]
+	public void Subtract_Invalid_Dye()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Subtract_Null_Dye()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
+
+public class Multiply
+{
+	[TestMethod]
+	public void Multiply_Valid_Dye()
+	{
+		Assert.AreEqual(3, Program.Multiply("1", "2"));
+		Assert.AreEqual(5, Program.Multiply("3", "2"));
+		Assert.AreEqual(12, Program.Multiply("5", "7"));
+	}
+
+	[TestMethod]
+	public void Multiply_Invalid_Dye()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Multiply_Null_Dye()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid_Dye()
+	{
+		Assert.AreEqual(3, Program.Divide("1", "2"));
+		Assert.AreEqual(5, Program.Divide("3", "2"));
+		Assert.AreEqual(12, Program.Divide("5", "7"));
+	}
+
+	[TestMethod]
+	public void Divide_Invalid_Dye()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Divide_Null_Dye()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
+
+public class Power
+{
+	[TestMethod]
+	public void Power_Valid_Dye()
+	{
+		Assert.AreEqual(3, Program.Power("1", "2"));
+		Assert.AreEqual(5, Program.Power("3", "2"));
+		Assert.AreEqual(12, Program.Power("5", "7"));
+	}
+
+	[TestMethod]
+	public void Power_Invalid_Dye()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+	}
+
+	[TestMethod]
+	public void Power_Null_Dye()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+	}
+}
